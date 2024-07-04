8467204081ea638ca0e059f1f3ae3682b5e2d83d
JohnDeeZimmermann <johndee.zimmermann@gmail.com>
2024-07-04 14:44:10 +0200

    Added a benchmark game

--- src/test/java/benchmarks/squares/SquareGame.java
+++ src/test/java/benchmarks/squares/SquareGame.java
@@ -1,7 +1,9 @@
 package benchmarks.squares;
 
+import de.cg.cgge.game.GameInstance;
 import de.cg.cgge.game.PhysicalGameObject;
 import de.cg.cgge.game.Room;
+import de.cg.cgge.physics.Mover;
 import de.cg.cgge.physics.shapes.CollisionBoxShape;
 import de.cg.cgge.physics.shapes.CollisionShape;
 
@@ -10,11 +12,21 @@ import java.awt.*;
 public class SquareGame {
 
     private class Square extends PhysicalGameObject {
+
+        private Mover mover;
+
         public Square(Room room, CollisionShape shape) {
             super(room, shape);
 
-            this.w = 50;
-            this.h = 50;
+            this.x = shape.getX();
+            this.y = shape.getY();
+            this.w = (int) shape.getWidth();
+            this.h = (int) shape.getHeight();
+
+            mover = new Mover(this);
+
+
+            addPhysicsComponent(mover);
         }
 
 
@@ -23,14 +35,29 @@ public class SquareGame {
             g.setColor(Color.RED);
             g.fillRect((int) x, (int) y,  w, h);
         }
+
+        @Override
+        public void step() {
+            mover.setXspeed(1);
+            mover.setYspeed(1);
+
+            mover.setYacceleration(1.01f);
+        }
     }
 
 
     public void measure() {
-        Room room = new Room(null);
-        Square square = new Square(room, new CollisionBoxShape(0, 0, 50, 50));
+        GameInstance game = new GameInstance();
 
+        Room room = new Room(game);
+        new Square(room, new CollisionBoxShape(0, 0, 50, 50));
 
+        game.changeRoomSafely(room);
+    }
+
+    public static void main(String args[]) {
+        SquareGame game = new SquareGame();
+        game.measure();
     }
 
 
